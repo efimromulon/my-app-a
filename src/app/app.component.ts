851073dc7761881e45fcdcd96d0a868b7393a034
ERomulon <efimromulonwork@gmail.com>
2020-05-17 18:14:14 +0300

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,15 +29,18 @@ import { HeroService } from './hero.service'
 
 
 export class AppComponent implements OnInit{
+
 	constructor(private heroService: HeroService) {};
+
 	title = 'Tour of Heroes';
 	heroes: Hero[];
 	selectedHero: Hero;
+
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
 	getHeroes(): void {
-		this.heroes = this.heroService.getHeroes();
+		this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
 	}
 	ngOnInit(): void {
 		this.getHeroes();
